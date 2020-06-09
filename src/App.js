26f97c17991d556beca9058c62483f5417f6ec43
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2020-06-09 16:14:12 -0300

    Carousel Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Carousel from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-
+        <Carousel></Carousel>
     </div>
   );
 }
