8348fedc79bf28b6dc5a878e547baae854c793df
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2020-06-09 16:22:13 -0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Carousel from "./Components/Carousel/Carousel";
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
         <Carousel></Carousel>
+        <Footer/>
     </div>
   );
 }
